10460c846cf75403da5b8fbbac72f29637f24e65
Ramziyamhd <115787142+Ramziyamhd@users.noreply.github.com>
2023-06-01 10:13:08 +0000

    ho modificato l'opacity

--- index.js
+++ index.js
@@ -2,4 +2,24 @@ function changeLanguage(language) {
     var languageButton = document.getElementById('language-button');
     languageButton.textContent = language;
   }
+
+  // Aggiungi questa parte solo se vuoi generare le carte dinamicamente
+// Puoi ripetere questo codice per ogni carta che vuoi generare
+const cardContainer = document.getElementById("card-container");
+
+const card = document.createElement("div");
+card.className = "polaroid-card";
+
+const image = document.createElement("img");
+image.src = "path_to_image.jpg";
+image.alt = "Immagine Polaroid";
+card.appendChild(image);
+
+const caption = document.createElement("div");
+caption.className = "caption";
+caption.textContent = "Testo della scritta";
+card.appendChild(caption);
+
+cardContainer.appendChild(card);
+
  
